57221eb1d2dcf6ea98af5df5794fa2617ed97f73
thiagocostamoreira <thiagocostamoreira@hotmail.com>
2024-02-29 19:50:47 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso da função
+let vitorias = 70;
+let derrotas = 20;
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
